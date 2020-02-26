05e23d18e5c6b738ce7cfbf841dc4c985143fba1
Jonny Sherwood-Bowes <jonnybowes2000@googlemail.com>
2020-02-26 17:32:42 +0000

    Commit..

--- Gucci/clam.sh
+++ Gucci/clam.sh
@@ -1,1 +1,2 @@
-sudo clamscan -ir --exclude-dir=^/sys --exclude-dir=^/dev --exclude-dir=^/proc && echo  && echo [  S C A N  C O M P L E T E  ] && echo 
+#!/bin/bash
+sudo clamscan -ir --exclude-dir=^/sys --exclude-dir=^/dev --exclude-dir=^/proc && echo  && echo [  S C A N  C O M P L E T E  ] && echo

56b118ed35939124814ec4c8ca3cde922bd40ba9
Jonny Sherwood-Bowes <jonnybowes2000@googlemail.com>
2020-04-30 08:30:33 +0100

    Commit..

--- Fedora/fedorainstall.sh
+++ Fedora/fedorainstall.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-sudo dnf update --refresh -y && \
+sudo dnf upgrade -y && \
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y && \
-sudo dnf install xorg-x11-drivers xorg-x11-xinit xorg-x11-drv-nvidia-340xx akmod-nvidia-340xx xorg-x11-drv-nvidia-340xx-cuda xorg-x11-drv-fbdev xorg-x11-drv-synaptics pulseaudio alsa-plugins-pulseaudio pulseaudio-module-x11 pulseaudio-utils kernel-modules-extra -y && \
-sudo dnf update -y && \
-sudo dnf install @base-x htop git wget curl gstreamer pavucontrol xbacklight powertop gvfs udiskie ffmpeg nano rxvt ncmpcpp libmpd dunst openbox i3lock dmenu feh lxappearance lxapperance-obconf lxmenu-data firefox tint2 conky mpv deja-dup gimp pidgin leafpad steam compton transmission darkplaces quake3 xclip evince asunder easytag simple-scan NetworkManager network-manager-applet nm-connection-editor NetworkManager-wifi VirtualBox virtualbox-guest-additions maim file-roller ubuntu-title-fonts adobe-source-code-pro-fonts terminus-fonts terminus-fonts-console -y && \ sudo dnf install lpf-spotify-client && \
+sudo dnf install -y install @base-x xorg-x11-server-Xorg xorg-x11-drv-{evdev,intel,synaptics} xorg-x11-xinit xterm terminus-fonts make automake gcc gcc-c++ kernel-devel git libX11-devel libXft-devel libXinerama-devel pulseaudio alsa-plugins-pulseaudio pulseaudio-module-x11 pulseaudio-utils kernel-modules-extra htop wget curl thunar gstreamer pavucontrol xbacklight powertop gvfs udiskie ffmpeg nano vim ncmpcpp libmpd dunst feh lxappearance firefox conky mpv gimp rsync leafpad steam compton transmission darkplaces quake3 xclip evince asunder easytag simple-scan NetworkManager network-manager-applet nm-connection-editor NetworkManager-wifi VirtualBox virtualbox-guest-additions maim file-roller ubuntu-title-fonts adobe-source-code-pro-fonts terminus-fonts terminus-fonts-console -y && \
+sudo dnf install lpf-spotify-client && \
 lpf approve spotify-client && \
 sudo -u pkg-build lpf build spotify-client && \
 sudo dnf install /var/lib/lpf/rpms/spotify-client/spotify-client-*.rpm && \
 sudo dnf install cups && \
 systemctl start cups && \
-echo 'exec openbox-session' >> ~/.xinitrc && \
+mkdir .src && cd .src && \
+git clone git://suckless.org/dwm && cd dwm && make && make install && cd .. && \
+git clone git://git.suckless.org/st && cd st && make && make install && cd .. && \
+git clone git://git.suckless.org/dmenu && cd dmenu && make && make install && cd .. && \
+git clone git://git.suckless.org/slock && cd slock && make && make install && cd .. && \
+touch ~/.xinitrc && echo 'exec dwm' >> ~/.xinitrc && \
 reboot

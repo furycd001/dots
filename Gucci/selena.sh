4d3e2037ba70f37ebe45a045b06db0959e275d7a
Jonny Sherwood-Bowes <jonnybowes2000@googlemail.com>
2020-05-27 14:24:17 +0100

    Commit..

---
+++ Gucci/selena.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+	yad --no-buttons --center --borders=22 --timeout=8 \
+		--title="BATTERY LOW. PLEASE PLUG IN CHARGER...." \
+		--image /home/furycd001/Terminal/Selena\ Gomez/batterylow.jpg \

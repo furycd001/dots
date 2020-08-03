77d856bf5bf9c77151479e69d06b1455550127aa
Jonny Sherwood-Bowes <jonnybowes2000@googlemail.com>
2020-08-03 08:21:55 +0100

    Commit..

--- Fedora/dwminstall.sh
+++ Fedora/dwminstall.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo dnf upgrade -y && \
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y && \
 sudo dnf install -y @base-x xorg-x11-server-Xorg xorg-x11-drv-{evdev,intel,synaptics,libinput} xorg-x11-xinit terminus-fonts make automake gcc gcc-c++ kernel-devel libX11-devel libXft-devel libXinerama-devel tlp tlp-rdw thermald cups pulseaudio alsa-plugins-pulseaudio pulseaudio-module-x11 pulseaudio-utils kernel-modules-extra libXrandr libXrandr-devel htop wget curl thunar pavucontrol xbacklight powertop gvfs udiskie ffmpeg nano vim ncmpcpp libmpd volumeicon dunst w3m w3m-img feh lxappearance firefox conky mpv gimp rsync leafpad steam compton transmission darkplaces quake3 xclip evince asunder easytag simple-scan NetworkManager network-manager-applet nm-connection-editor NetworkManager-wifi VirtualBox virtualbox-guest-additions maim file-roller ubuntu-title-fonts adobe-source-code-pro-fonts terminus-fonts terminus-fonts-console -y && \
 mkdir /home/furycd001/.src/ && cd /home/furycd001/.src/ && \
@@ -16,8 +15,11 @@ cd dwm-6.2 && make && sudo make install && cd .. &&
 cd st-0.8.3 && make && sudo make install && cd .. && \
 cd dmenu-4.9 && make && sudo make install && cd .. && \
 cd slock-1.4 && make && sudo make install && cd .. && \
+cd /etc/conky/ && \
+wget https://raw.githubusercontent.com/furycd001/dots/master/conky/output-bar.conf && \
+mv output-bar.conf conky.conf && \
 cd /home/furycd001/ && \
-mkdir Pictures/ Documents/ Downloads/ Sites/ Music/ Videos/ && \
+mkdir Pictures/ Documents/ Downloads/ Sites/ Music/ Videos/ Terminal/ && \
 wget https://i.imgur.com/nIP3YDW.jpg && \
 cp nIP3YDW.jpg Coffee.jpg && \
 cd /home/furycd001/ && \

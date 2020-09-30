87d3749c8b38ede1624d58f23e9e73e81b2c8023
Jonny Sherwood-Bowes <jonnybowes2000@googlemail.com>
2020-09-30 08:19:11 +0100

    Commit..

--- Gucci/conkyterminal.sh
+++ Gucci/conkyterminal.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+xdotool getwindowfocus windowmove 2948 22
 clear && \
-watch -t -n 22 'conky -q -c /home/furycd001/Dots/conky/terminal.conf' && \
+watch -t -n 1 'conky -q -c /home/furycd001/Dots/conky/terminal.conf' && \
 echo  && \
 exit 0;

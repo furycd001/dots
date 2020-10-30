1c548c8f95118e3404d0a1890250f1baf0f5cea9
Jonny Sherwood-Bowes <jonnybowes2000@googlemail.com>
2020-10-30 13:12:26 +0000

    Commit..

--- Gucci/newsboat.sh
+++ Gucci/newsboat.sh
@@ -1,19 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-xfce4-terminal --hide-menubar --geometry=62x46 --execute newsboat &
-
-# Process ID of the process we just launched
-PID=$!
-
-# Window ID of the process...pray that there's
-# only one window! Otherwise this might break.
-# We also need to wait for the process to spawn
-# a window.
-while [ "$WID" == "" ]; do
-        WID=$(wmctrl -lp | grep $PID | cut "-d " -f1)
-done
-# Set the size and location of the window
-# See man wmctrl for more info
-wmctrl -i -r $WID -e 0,2790,18,478,734
-
-exit 0
+yad --no-buttons --center --borders=22 --timeout=22 --title="Newsboat..." --text="[ N E W S B O A T ]" \
+--field="Youtube":fbtn "xfce4-terminal --hide-menubar --geometry=82x43 --execute newsboat -u /home/furycd001/.newsboat/youtube" \
+--field="Rss":fbtn "xfce4-terminal --hide-menubar --geometry=82x43 --execute newsboat -u /home/furycd001/.newsboat/rss" \
+--field="Podcast":fbtn "xfce4-terminal --hide-menubar --geometry=82x43 --execute newsboat -u /home/furycd001/.newsboat/podcast" \

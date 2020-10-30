96a2ee83de9acd2b43594c1206d872acdfdfe884
Jonny Sherwood-Bowes <jonnybowes2000@googlemail.com>
2020-10-30 14:31:48 +0000

    Commit..

--- Gucci/newsboat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-yad --form --center --borders=22 --timeout=22 --title="Newsboat..." --text="[ N E W S B O A T ]" \
---field="Youtube":fbtn "xfce4-terminal --hide-menubar --geometry=82x43 --execute newsboat -u /home/furycd001/.newsboat/youtube" \
---field="Rss":fbtn "xfce4-terminal --hide-menubar --geometry=82x43 --execute newsboat -u /home/furycd001/.newsboat/rss" \
---field="Podcast":fbtn "xfce4-terminal --hide-menubar --geometry=82x43 --execute newsboat -u /home/furycd001/.newsboat/podcast" \
---button=gtk-close:1

ddecc6adb615ecc1c057a4aa95eafe636535b8dd
Jonny Sherwood-Bowes <jonnybowes2000@googlemail.com>
2021-01-11 14:16:19 +0000

    Duplicated conkyterminal.sh....
    Duplicated conkyterminal.sh....

---
+++ Gucci/conkyterminal1.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Laptop //
+xdotool getwindowfocus windowmove 1588 22
+# External monitor //
+# xdotool getwindowfocus windowmove 2948 22
+clear && \
+watch -t -n 1 'conky -q -c /home/furycd001/Dots/conky/terminal.conf' && \
+exit 0;

cd2000c064309fa1a01ddfe7af9a5780d105843e
Jonny Sherwood-Bowes <jonnybowes2000@googlemail.com>
2021-01-20 11:54:41 +0000

    Created video wallpaper script....
    https://furycd001.github.io/video-as-desktop-wallpaper/

---
+++ Gucci/videowall.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#set video as wallpaper using xwinwrap and mpv - change path to your video!! Do not use my default settings
+
+xwinwrap -ni -fs -s -st -sp -b -nf -- mpv --profile wallpaper --wid WID --shuffle /home/furycd001/Terminal/Anastasiya\ Mehovaya/
+
+# /home/furycd001/Terminal/Rebecca\ More/Moremilf\ Videos/XCTUbPl4VIiEU7VK.mp4

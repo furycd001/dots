422d8e0de6924fc6c6d3bc9b3eee21b0ced535be
Jonny Sherwood-Bowes <jonnybowes2000@googlemail.com>
2021-01-25 08:46:21 +0000

    Updated arc firefox theme, battery.sh & videowall.sh
    Added a fix for out of place menu entries in arc firefox. Changed battery image in battery script & added sleep to videowall script....

--- Gucci/videowall.sh
+++ Gucci/videowall.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #set video as wallpaper using xwinwrap and mpv - change path to your video!! Do not use my default settings
 
+sleep 4s && \
 xwinwrap -ni -fs -s -st -sp -b -nf -- mpv --profile wallpaper --wid WID --shuffle /home/furycd001/Terminal/Anastasiya\ Mehovaya/
 
 # /home/furycd001/Terminal/Rebecca\ More/Moremilf\ Videos/XCTUbPl4VIiEU7VK.mp4

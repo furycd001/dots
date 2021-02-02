d72f20a20f99a28639c4277502925f751034ab71
Jonny Sherwood-Bowes <jonnybowes2000@googlemail.com>
2021-02-02 15:12:24 +0000

    Updated fetch.sh....
    Reworked fetch.sh....

--- Gucci/fetch.sh
+++ Gucci/fetch.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-echo -ne ${bold}OS:${normal}  && lsb_release -ds
-echo -ne ${bold}KR:${normal}  && uname -r
-echo -ne ${bold}UP:${normal}  && uptime -p
-echo -ne ${bold}SH:${normal}  && echo $SHELL
-echo -ne ${bold}RS:${normal}  && xdpyinfo | awk '/dimensions/{print $2}'
+os=$(lsb_release -ds)
+kr=$(uname -r)
+up=$(uptime -p)
+sh=$(echo $SHELL)
+rs=$(xdpyinfo | awk '/dimensions/{print $2}')
+
+echo -ne ${bold}OS:${normal} && echo $os
+echo -ne ${bold}KR:${normal} && echo $kr
+echo -ne ${bold}UP:${normal} && echo $up
+echo -ne ${bold}SH:${normal} && echo $sh
+echo -ne ${bold}RS:${normal} && echo $rs
 
 f=3 b=4 g=9
 for j in f b g; do
@@ -15,12 +21,12 @@ done
 d=$'\e[1m'
 t=$'\e[0m'
 v=$'\e[7m'
- 
- 
+
+
 cat << EOF
- 
- $f1███$d$g1▄$t  $f2███$d$g2▄$t  $f3███$d$g3▄$t  $f4███$d$g4▄$t  $f5███$d$g5▄$t  $f6███$d$g6▄$t  $f7███$d$g7▄$t  
- $f1███$d$g1█$t  $f2███$d$g2█$t  $f3███$d$g3█$t  $f4███$d$g4█$t  $f5███$d$g5█$t  $f6███$d$g6█$t  $f7███$d$g7█$t  
- $f1███$d$g1█$t  $f2███$d$g2█$t  $f3███$d$g3█$t  $f4███$d$g4█$t  $f5███$d$g5█$t  $f6███$d$g6█$t  $f7███$d$g7█$t  
- $d$g1 ▀▀▀   $g2▀▀▀   $g3▀▀▀   $g4▀▀▀   $g5▀▀▀   $g6▀▀▀   $g7▀▀▀  
+
+ $f1███$d$g1▄$t  $f2███$d$g2▄$t  $f3███$d$g3▄$t  $f4███$d$g4▄$t  $f5███$d$g5▄$t  $f6███$d$g6▄$t  $f7███$d$g7▄$t
+ $f1███$d$g1█$t  $f2███$d$g2█$t  $f3███$d$g3█$t  $f4███$d$g4█$t  $f5███$d$g5█$t  $f6███$d$g6█$t  $f7███$d$g7█$t
+ $f1███$d$g1█$t  $f2███$d$g2█$t  $f3███$d$g3█$t  $f4███$d$g4█$t  $f5███$d$g5█$t  $f6███$d$g6█$t  $f7███$d$g7█$t
+ $d$g1 ▀▀▀   $g2▀▀▀   $g3▀▀▀   $g4▀▀▀   $g5▀▀▀   $g6▀▀▀   $g7▀▀▀
 EOF

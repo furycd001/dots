05e40006ff7d1d1710f76e3231b6080adbb9a32b
Jonny Sherwood-Bowes <jonnybowes2000@googlemail.com>
2021-02-11 21:12:25 +0000

    Updated conkyterminal scripts & readme....
    Hid cursor in conky terminal windows....

--- Gucci/conkyterminal.sh
+++ Gucci/conkyterminal.sh
@@ -5,5 +5,6 @@
 # External monitor //
 xdotool getwindowfocus windowmove 2948 22
 clear && \
+tput civis && \
 watch -t -n 1 'conky -q -c /home/furycd001/Dots/conky/terminal.conf' && \
 exit 0;
--- Gucci/info.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-timeout 0.1s  conky -q -c /home/furycd001/Dots/conky/notify.conf > /tmp/notify.txt && \
-notify-send -t 4000 "$(cat /tmp/notify.txt)" && \
-sleep 4s && \
-rm /tmp/notify.txt

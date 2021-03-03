6ff47c817441cd45e085a750048c8944ade6b2ca
Jonny Sherwood-Bowes <jonnybowes2000@googlemail.com>
2021-03-03 07:18:56 +0000

    Updated files in fedorakde2....
    setup.sh modified....

--- fedorakde2/setup.sh
+++ fedorakde2/setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 dnf install \
-bison bison-devel cmake cnmake-fedora extra-cmake-modules byacc flex flex-devel \
+bison bison-devel cmake cmake-fedora extra-cmake-modules byacc flex flex-devel \
 freeglut freeglut-devel libacl libacl-devel alsa-lib bzip2-devel bzip2-libs \
-freetype-devel mesa-libGL-devel libjpeg-turbo libjpeg-turbo-deve1 mingw32-libltdl \
+freetype-devel mesa-libGL-devel libjpeg-turbo libjpeg-turbo-devel mingw32-libltdl \
 libmng-devel pcre-devel libtiff-devel libXft libXft-devel libxml2-devel libXmu-devel \
 libxslt-devel pkgconf-pkg-config pkgconf

52808e12fa797f2e2c6405952f608c1f47f9f788
Jonny Sherwood-Bowes <jonnybowes2000@googlemail.com>
2021-04-27 10:06:04 +0100

    Updated files....
    Files updated....

--- Gucci/videowall.sh
+++ Gucci/videowall.sh
@@ -3,9 +3,10 @@
 
 
 sleep 4s && \
-xwinwrap -ni -fs -s -st -sp -b -nf -- mpv --profile=wallpaper --shuffle -wid WID /home/furycd001/Terminal/Moonchild/vwall/
+xwinwrap -ni -fs -s -st -sp -b -nf -- mpv --profile=wallpaper --shuffle -wid WID /home/furycd001/Downloads/videowall.m3u
 
 
-# /home/furycd001/Terminal/Samantha\ De\ Reviziis/vwall/Kastoria.mp4
-# /home/furycd001/Terminal/Samantha\ De\ Reviziis/vwall/Kopenhagen.mp4
-# /home/furycd001/Terminal/Samantha\ De\ Reviziis/vwall/Saga.mp4
+# /home/furycd001/Terminal/MSI/MSI\ Videos/
+# /home/furycd001/Terminal/Anastasiya Mehovaya/Anastasiya\ Mehovaya\ Videos/
+# /home/furycd001/Terminal/Moonchild/vwall/
+# /home/furycd001/Terminal/Samantha\ De\ Reviziis/vwall/

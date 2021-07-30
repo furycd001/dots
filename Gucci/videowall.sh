7f4905e368fc11386dda5e1418605ec618b81142
Jonny <jonnybowes2000@googlemail.com>
2021-07-30 09:09:09 +0100

    Updated userchromes & cleaned up files....
    Updated arc-proton & single userchromes, along with cleaning up some files....

--- Gucci/videowall.sh
+++ Gucci/videowall.sh
@@ -3,12 +3,12 @@
 
 
 sleep 4s && \
-xwinwrap -ni -fs -s -st -sp -b -nf -- mpv --profile=wallpaper --shuffle -wid WID ~/Terminal/vwall/
+xwinwrap -ni -fs -s -st -sp -b -nf -- mpv --profile=wallpaper --shuffle -wid WID /run/media/furycd001/Reviziis/Fur/Instagram/Fursberry/Videos/
 
 
+#~/Terminal/vwall/
 # ~/Pictures/vwall/ArianaGrandeVogue.mp4
 # /home/furycd001/Terminal/MSI/MSI\ Videos/
 # /home/furycd001/Terminal/Anastasiya Mehovaya/Anastasiya\ Mehovaya\ Videos/
 # /home/furycd001/Terminal/Moonchild/vwall/
 # /home/furycd001/Terminal/Samantha\ De\ Reviziis/vwall/
-

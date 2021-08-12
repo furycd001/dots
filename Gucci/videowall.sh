71219be76fa23b465e543f8ef7112cd4e9489582
Jonny <jonnybowes2000@googlemail.com>
2021-08-12 13:47:08 +0100

    Updated files....
    Some files have been updated....

--- Gucci/videowall.sh
+++ Gucci/videowall.sh
@@ -3,10 +3,4 @@
 
 
 sleep 4s && \
-xwinwrap -ni -fs -s -st -sp -b -nf -- mpv --profile=wallpaper --shuffle -wid WID ~/Pictures/vwall/ArianaGrandeVogue.mp4
-
-
-# /home/furycd001/Reviziis/Fur/Instagram/Moonchild/vwall/
-# /run/media/furycd001/Reviziis/Fur/Instagram/Fursberry/Videos/
-# ~/Pictures/vwall/ArianaGrandeVogue.mp4
-# /home/furycd001/Terminal/Samantha\ De\ Reviziis/vwall/
+xwinwrap -ni -fs -s -st -sp -b -nf -- mpv --profile=wallpaper --shuffle -wid WID ~/Pictures/vwall/

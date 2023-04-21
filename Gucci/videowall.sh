9e0f745172e2772fc6ba331dc8c2fcb1e15e8e4e
furycd001 <jonnybowes2000@googlemail.com>
2023-04-21 17:52:47 +0100

    Fixed a heading in icon readme....

--- Gucci/videowall.sh
+++ Gucci/videowall.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 #set video as wallpaper using xwinwrap and mpv - change path to your video!! Do not use my default settings
+# Using pgrep and pkill, we can check to see if MPV is running and, if so, kill it or start it.
+# There's probably a better way of doing this because you could maybe have more than one instance of MPV open and this way would kill them all.
+# It works though....
 
-
-sleep 4s && \
-xwinwrap -ni -fs -s -st -sp -b -nf -- mpv --profile=wallpaper -wid %WID /home/furycd001/Terminal/Anastasiya/
+ if pgrep mpv; then
+    pkill mpv; else
+    xwinwrap -ni -fs -s -st -sp -b -nf -- mpv --profile=wallpaper -wid %WID /home/furycd001/Terminal/
+  fi
--- Icons/readme.md
+++ Icons/readme.md
@@ -1,4 +1,4 @@
-# [ CONKY ]
+# [ ICONS ]
 
 ## Arc-OSX-V..
 

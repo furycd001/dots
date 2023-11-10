9eabb97b8e13e6469443f2479ff175de22bd7361
furycd001 <jonnybowes2000@googlemail.com>
2023-11-10 16:44:21 +0000

    Updated frames.sh....

--- Gucci/frames.sh
+++ Gucci/frames.sh
@@ -2,9 +2,11 @@
 
 mkdir -p output_frames
 
-for file in *.mp4; do
-    filename=$(basename -- "$file")
-    filename="${filename%.*}"
-    mkdir -p "output_frames/$filename"
-    ffmpeg -i "$file" "output_frames/$filename/image_%04d.png"
+for file in *.{mp4,webm}; do
+    if [ -e "$file" ]; then
+        filename=$(basename -- "$file")
+        filename="${filename%.*}"
+        mkdir -p "output_frames/$filename"
+        ffmpeg -i "$file" "output_frames/$filename/image_%04d.png"
+    fi
 done

1db17fcc243896b8198420c2dad32dc7449e2653
furycd001 <jonnybowes2000@googlemail.com>
2024-02-08 10:54:34 +0000

    Updated todo.sh & fixed issue with mnml conky....

--- Gucci/todo.sh
+++ Gucci/todo.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sleep 4s && \
-notify-send -u critical -t 4000 "$(cat /home/furycd001/Documents/todo.txt)"
+notify-send -u critical -t 4000 "$(fold -s -w 50 /home/furycd001/Documents/todo.txt)"
